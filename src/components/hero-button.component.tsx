753fc2e0ea6053bd366f0b90a70f1a9d75cf791f
Nikolai Morozov <nikolaj.morozov@rsquad.io>
2022-12-11 22:45:00 +0300

    hero-button done

--- src/components/hero-button.component.tsx
+++ src/components/hero-button.component.tsx
@@ -9,8 +9,8 @@ type Props = {
 export const HeroButton: FC<Props> = ({ text, image }) => {
   return (
     <div className="hero-button">
-      <div className="hero-button__text">{text}</div>
       <img src={image} alt="" className="hero-button__img" />
+      <div className="hero-button__text">{text}</div>
     </div>
   );
 };
