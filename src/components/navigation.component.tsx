d1979da47f6ed3976cbf83b672b2f1c3efd7ae8d
Nikolai Morozov <nikolaj.morozov@rsquad.io>
2022-12-11 16:31:39 +0300

    navigation done

--- src/components/navigation.component.tsx
+++ src/components/navigation.component.tsx
@@ -1,10 +1,11 @@
 import { Menu } from "./menu.component";
+import iLogo from "../assets/logo.svg";
 import "./navigation.component.sass";
 
 export const Navigation = () => {
   return (
     <div className="navigation">
-      <div className="navigation__title">ВЕНАРУС</div>
+      <img src={iLogo} className="navigation__logo" />
       <div className="navigation__right">
         <div className="navigation__btn">Где купить</div>
         <Menu />
