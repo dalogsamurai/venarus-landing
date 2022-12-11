ce9b1cd40233c5524c7235ec2a174c3bf0f38e5a
Nikolai Morozov <nikolaj.morozov@rsquad.io>
2022-12-11 03:47:06 +0300

    vars and menu content

--- src/components/menu.component.tsx
+++ src/components/menu.component.tsx
@@ -1,5 +1,13 @@
 import "./menu.component.sass";
 
 export const Menu = () => {
-  return <div className="menu"></div>;
+  return (
+    <div className="menu">
+      <div className="menu__item">Где купить</div>
+      <div className="menu__item">О препарате</div>
+      <div className="menu__item">О варикозе</div>
+      <div className="menu__item">Решение</div>
+      <div className="menu__item">Специалистам</div>
+    </div>
+  );
 };
