93aacc4e2325a29daa477f5ce08632ce87d1faf9
Nikolai Morozov <nikolaj.morozov@rsquad.io>
2022-12-13 22:24:10 +0300

    switcher wip

--- src/components/switcher.component.tsx
+++ src/components/switcher.component.tsx
@@ -1,14 +1,19 @@
 import { useState } from "react";
-import iHeroBackground from "../assets/hero-background.svg";
+import iVenarus1 from "../assets/venarus-1.svg";
+import iVenarus2 from "../assets/venarus-2.svg";
+import iVenarus3 from "../assets/venarus-3.svg";
 import iArrow from "../assets/arrow.svg";
 import "./switcher.component.sass";
 
 export const Switcher = () => {
+  const items = [iVenarus1, iVenarus2, iVenarus3];
   const [selected, isSelected] = useState(0);
 
   return (
     <div className="switcher">
-      <div className="switcher__content"></div>
+      <div className="switcher__content">
+        <img src={items[selected]} alt="" className="switcher__item" />
+      </div>
       <div className="switcher__buttons">
         <img className="switcher__arrow" src={iArrow} />
         <img className="switcher__arrow right" src={iArrow} />
