29ae503145628aa4536c0cc1b67cd8086dbd7ca7
Nikolai Morozov <nikolaj.morozov@rsquad.io>
2022-12-13 23:45:42 +0300

    wip

--- src/components/switcher.component.tsx
+++ src/components/switcher.component.tsx
@@ -3,23 +3,30 @@ import iVenarus1 from "../assets/venarus-1.svg";
 import iVenarus2 from "../assets/venarus-2.svg";
 import iVenarus3 from "../assets/venarus-3.svg";
 import iArrow from "../assets/arrow.svg";
+import cn from "classnames";
 import "./switcher.component.sass";
 
 export const Switcher = () => {
   const items = [iVenarus1, iVenarus2, iVenarus3];
-  const [selected, isSelected] = useState(0);
+  const [selected, setSelected] = useState(0);
 
   return (
     <div className="switcher">
       <div className="switcher__content">
-        <img src={items[selected]} alt="" className="switcher__item" />
+        {items.map((item, i) => (
+          <img
+            src={item}
+            alt=""
+            className={cn("switcher__item", { _active: selected === i })}
+          />
+        ))}
       </div>
       <div className="switcher__buttons">
         <img
           className="switcher__arrow"
           onClick={() => {
             if (selected !== 0) {
-              isSelected(selected - 1);
+              setSelected(selected - 1);
             }
           }}
           src={iArrow}
@@ -28,7 +35,7 @@ export const Switcher = () => {
           className="switcher__arrow right"
           onClick={() => {
             if (selected !== items.length - 1) {
-              isSelected(selected + 1);
+              setSelected(selected + 1);
             }
           }}
           src={iArrow}
