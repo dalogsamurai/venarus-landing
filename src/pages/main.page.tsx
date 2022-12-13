7a693629ab21df115350de5956673e16d4379da4
Nikolai Morozov <nikolaj.morozov@rsquad.io>
2022-12-13 23:58:27 +0300

    layout to main page

--- src/pages/main.page.tsx
+++ src/pages/main.page.tsx
@@ -2,13 +2,17 @@ import { Hero } from "../components/hero.component";
 import { Navigation } from "../components/navigation.component";
 import iHeroBackground from "../assets/hero-background.svg";
 import "./main.page.sass";
+import { Disclaimer } from "../components/disclaimer.component";
 
 export const MainPage = () => {
   return (
     <>
-      <div className="main-page">
+      <div className="main-page layout">
         <Navigation />
         <Hero />
+      </div>
+      <div className="main-page">
+        <Disclaimer />
       </div>
       <img src={iHeroBackground} alt="" className="background" />
     </>
