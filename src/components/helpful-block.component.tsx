7a6258c7435ba47b3773a71f29d1a25574d87a9f
Nikolai Morozov <nikolaj.morozov@rsquad.io>
2022-12-14 12:51:25 +0300

    helpful wip

--- src/components/helpful-block.component.tsx
+++ src/components/helpful-block.component.tsx
@@ -1,5 +1,16 @@
+import { FC } from "react";
 import "./helpful-block.component.sass";
 
-export const HelpfulBlock = () => {
-  return <div className="helpful-block"></div>;
+type Props = {
+  text: string;
+  image?: any;
+};
+
+export const HelpfulBlock: FC<Props> = ({ text, image }) => {
+  return (
+    <div className="helpful-block">
+      <div className="helpful-block__text">{text}</div>
+      {image && <img src={image} alt="" className="helpful-block__image" />}
+    </div>
+  );
 };
--- src/components/helpful.component.tsx
+++ src/components/helpful.component.tsx
@@ -1,5 +1,16 @@
+import iHelpful1 from "../assets/helpful-1.jpg";
+import iHelpful2 from "../assets/helpful-2.jpeg";
+import iHelpful3 from "../assets/helpful-3.jpg";
+import { HelpfulBlock } from "./helpful-block.component";
 import "./helpful.component.sass";
 
 export const Helpful = () => {
-  return <div className="helpful"></div>;
+  return (
+    <div className="helpful">
+      <HelpfulBlock image={iHelpful1} text="Профилактические упражнения" />
+      <HelpfulBlock image={iHelpful2} text="Диагностика и способы лечения" />
+      <HelpfulBlock image={iHelpful3} text="Планы по питанию " />
+      <HelpfulBlock text="Факты о варикозе" />
+    </div>
+  );
 };
