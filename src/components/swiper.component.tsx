277b128e46c7a02c70522dbc2075e9a41ec7052c
Nikolai Morozov <nikolaj.morozov@rsquad.io>
2022-12-14 11:07:54 +0300

    wip

--- src/components/swiper.component.tsx
+++ src/components/swiper.component.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import iVideoPreview1 from "../assets/video-preview-1.svg";
 import iVideoPreview2 from "../assets/video-preview-2.png";
 import iVideoPreview3 from "../assets/video-preview-3.jpg";
+import cn from "classnames";
 import "./swiper.component.sass";
 
 export const Swiper = () => {
@@ -23,9 +24,18 @@ export const Swiper = () => {
     <div className="swiper">
       <img src={items[active]} alt="" className="swiper__image" />
       <div className="swiper__buttons">
-        <div className="swiper__button" onClick={() => setActive(0)} />
-        <div className="swiper__button" onClick={() => setActive(1)} />
-        <div className="swiper__button" onClick={() => setActive(2)} />
+        <div
+          className={cn("swiper__button", { active: active === 0 })}
+          onClick={() => setActive(0)}
+        />
+        <div
+          className={cn("swiper__button", { active: active === 1 })}
+          onClick={() => setActive(1)}
+        />
+        <div
+          className={cn("swiper__button", { active: active === 2 })}
+          onClick={() => setActive(2)}
+        />
       </div>
     </div>
   );
